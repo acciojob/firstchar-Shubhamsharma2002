085aef196e7955a3f35fcb60e5731f9316368307
Shubham Sharma <142394297+Shubhamsharma2002@users.noreply.github.com>
2026-06-16 18:22:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 function firstChar(text) {
   // your code here
+  // Remove leading spaces
+  const trimmedText = text.trimStart();
+  
+  // If the string is empty or only had spaces, return an empty string
+  if (trimmedText.length === 0) {
+    return '';
+  }
+  
+  // Return the very first character of the remaining text
+  return trimmedText[0];
 }
 
 // Do not change the code below
 //Uncomment the following line to show the prompt popup
-//const text = prompt("Enter text:");
+const text = prompt("Enter text:");
 alert(firstChar(text));
